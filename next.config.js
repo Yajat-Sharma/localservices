5878a89fa685980483b562ae69de909abfe43547
Yajat Sharma <yajats@gmail.com>
2026-03-15 18:17:34 +0530

    Fix config

--- next.config.js
+++ next.config.js
@@ -17,4 +17,29 @@ const nextConfig = {
     serverComponentsExternalPackages: ["@prisma/client", "prisma"],
   },
 };
+
+module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com", 
+      "firebasestorage.googleapis.com",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+  outputFileTracingIncludes: {
+    "/api/**": ["./node_modules/@prisma/client/**"],
+  },
+};
 module.exports = nextConfig;
